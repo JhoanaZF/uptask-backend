3c7abf50ac5de433530cb55a80bf78a6336c3919
jhoana <jhoanazamora2@gmail.com>
2024-10-20 21:16:07 -0500

    añadiendo colors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import colors from 'colors'
 import server from './server'
 
 const port = process.env.PORT || 4000
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+  console.log(colors.cyan.bold(`Server is running on port ${port}`))
 })
